a054d3b092a0e58d42de886b109a20e40ae03b28
Arturo <atercerosb@univalle.edu>
2021-02-20 08:41:41 -0400

    register validation

--- src/views/Auth/Register.js
+++ src/views/Auth/Register.js
@@ -7,9 +7,12 @@ const Register = () => {
   //extraer los valores del context
   const authContext = useContext(AuthContext);
   const { mensaje, autenticado, registrarUsuario } = authContext;
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState({
+            message: '',
+            type: ''
+        });
   const history = useHistory();
-  // State para iniciar sesión
+  // Statepara iniciar sesión
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -17,13 +20,26 @@ const Register = () => {
     confirmar: '',
   });
   const { nombre, email, password, confirmar } = user;
+   const [formValid, setFormValid] = useState({
+            name: false,
+            email: false,
+            password: false,
+            confirmar:false
+        });
+  const initialErrorMessageState = {
+            mesage: '',
+            type: ''
+        };
   //En caso que el usuario se haya autenticado / registrado o sea un registro duplicado
   useEffect(() => {
     if (autenticado) {
       history.push('/');
     }
     if (mensaje) {
-      setErrorMessage(mensaje);
+      setErrorMessage({
+          message: mensaje,
+          type: 'usuario'
+      });
     }
   }, [mensaje, autenticado, history]);
 
@@ -41,9 +57,116 @@ const Register = () => {
     });
   };
 
-  if (autenticado) {
-    return <Redirect to='/' />;
+  const validateEmail = (email) => {
+            const EMAIL_REGEX = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+            const emailIsValid = EMAIL_REGEX.test(email)
+            if (emailIsValid) {
+                setFormValid({
+                    ...formValid,
+                    email: true
+                })
+                setErrorMessage({...initialErrorMessageState })
+            } else {
+                setErrorMessage({
+                    message: 'Su email es incorrecto',
+                    type: 'email'
+                })
+                setFormValid({
+                    ...formValid,
+                    email: false
+                })
+            }
+        }
+
+  const validateConfirmar=(confirmar)=>{
+    if(confirmar===password){
+      setFormValid({
+        ...formValid,
+        confirmar: true
+      })
+      setErrorMessage({...initialErrorMessageState })
+    }else{
+      setErrorMessage({
+        message: 'No coinciden sus contraseñas',
+        type: 'confirmar'
+      })
+      setFormValid({
+          ...formValid,
+          confirmar: false
+      })
+    }
   }
+
+  const handleBlur = (e) => {
+            const { target } = e;
+            const { value, name } = target;
+            let message
+            if (value.length === 0) {
+              switch (name) {
+                case 'name':
+                  setFormValid({
+                        ...formValid,
+                        name: false
+                  });
+                  message=`Ingrese su nick`
+                  break;
+                case 'email':
+                  setFormValid({
+                        ...formValid,
+                        email: false
+                  });
+                  message=`Ingrese su ${name}`
+                  break;
+                case 'password':
+                  setFormValid({
+                        ...formValid,
+                        password: false
+                  });
+                  message=`Ingrese su ${name}`
+                  break;
+                case 'confirmar':
+                  setFormValid({
+                        ...formValid,
+                        confirmar: false
+                  });
+                  message=`Confirme su password`
+                  break;
+              }
+                
+              return setErrorMessage({
+                  message: message,
+                  type: name
+              })
+            }
+            
+            switch (name) {
+              case 'name':
+                setFormValid({
+                    ...formValid,
+                    name: true
+                });
+                setErrorMessage({...initialErrorMessageState });
+                break;
+              case 'email':
+                validateEmail(value)
+                break;
+              case 'password':
+                setFormValid({
+                    ...formValid,
+                    password: true
+                });
+                setErrorMessage({...initialErrorMessageState });
+                break;
+              case 'confirmar':
+                validateConfirmar(value)
+                break;
+            }
+           
+            
+        }
+        if (autenticado) {
+          return <Redirect to='/' />;
+        }
 
   return (
     <div className='Register'>
@@ -52,7 +175,7 @@ const Register = () => {
         <span className='title__message'>¡Vamos a registrarte!</span>
       </h1>
       <form className='Register__form' onSubmit={signIn}>
-        {errorMessage ? <p>{errorMessage}</p> : ''}
+        {errorMessage.message ? <p>{errorMessage.message}</p> : ''}
         <div className='form__group_re'>
           <label htmlFor='name' className='form__label_re'>
             Nick:
@@ -63,6 +186,8 @@ const Register = () => {
             value={user.name}
             className='form__input_re'
             onChange={handleInputChange}
+            onBlur = { handleBlur }
+            className = { errorMessage.type === 'name' ? 'error' : '' }
           />
         </div>
         <div className='form__group_re'>
@@ -75,6 +200,8 @@ const Register = () => {
             value={user.email}
             className='form__input_re'
             onChange={handleInputChange}
+            onBlur = { handleBlur }
+            className = { errorMessage.type === 'email' ? 'error' : '' }
           />
         </div>
         <div className='form__group_re'>
@@ -87,16 +214,28 @@ const Register = () => {
             value={user.password}
             className='form__input_re'
             onChange={handleInputChange}
+            onBlur = { handleBlur }
+            className = { errorMessage.type === 'password' ? 'error' : '' }
           />
         </div>
         <div className='form__group_re'>
           <label htmlFor='password' className='form__label_re'>
             Confirmar Contraseña:
           </label>
-          <input name='password' type='password' className='form__input_re' />
+          <input name='confirmar' 
+          type='password' 
+          className='form__input_re' 
+          onChange={handleInputChange}
+          onBlur = { handleBlur }
+          className = { errorMessage.type === 'confirmar' ? 'error' : '' }
+          />
         </div>
         <div className='form__group_re'>
-          <button type='submit' className='form__button_re'>
+          <button 
+          type='submit' 
+          className='form__button_re'
+          disabled={!(formValid.name&&formValid.email&&formValid.password&&formValid.confirmar)}
+          >
             Registrarse
           </button>
           <label className='form__register_re'>
