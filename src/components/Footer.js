f93a220f020b2c2bd838de07d4b9de404e69646b
NardaC <nardac6@gmail.com>
2021-02-20 16:49:26 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from '../assets/img/Logo2.png';
-import '../assets/styles/components/Footer.css';
+import React from "react";
+import logo from "../assets/img/Logo2.png";
+import "../assets/styles/components/Footer.css";
+
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <img src={logo} className='logo-footer' alt='logo' />
+    <div className="footer">
+        <img src={logo} className="logo-footer" alt="logo" />
     </div>
   );
 };
