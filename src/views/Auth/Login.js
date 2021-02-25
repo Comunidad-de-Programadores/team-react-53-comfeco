9de52b99528552a8f23095e0c3119eaa43bba521
NardaC <nardac6@gmail.com>
2021-02-25 04:44:10 -0500

    login

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,12 +1,11 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import {
-  loginWithGoogle,
-  loginWithFacebook,
-} from '../../firebase/client';
-import AuthContext from '../../auth/AuthContext';
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { loginWithGoogle, loginWithFacebook } from "../../firebase/client";
+import AuthContext from "../../auth/AuthContext";
 // import { types } from '../../types/types';
-import '../../assets/styles/views/Login.css';
+import "../../assets/styles/views/Login.css";
+
+import fondo from "../../assets/img/fondo.png"
 
 const Login = () => {
   const authContext = useContext(AuthContext);
@@ -18,20 +17,20 @@ const Login = () => {
     loginFacebook,
   } = authContext;
   const [errorMessage, setErrorMessage] = useState({
-    message: '',
-    type: '',
+    message: "",
+    type: "",
   });
   const [user, setUser] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const [formValid, setFormValid] = useState({
     email: false,
     password: false,
   });
   const initialErrorMessageState = {
-    mesage: '',
-    type: '',
+    mesage: "",
+    type: "",
   };
 
   const history = useHistory();
@@ -40,15 +39,14 @@ const Login = () => {
   const { email, password } = user;
 
   //En caso de que el password o usuario no exista
-
   useEffect(() => {
     if (autenticado) {
-      history.replace('/');
+      history.replace("/");
     }
     if (mensaje) {
       setErrorMessage({
         message: mensaje,
-        type: 'usuario',
+        type: "usuario",
       });
     }
   }, [mensaje, autenticado, history]);
@@ -57,7 +55,7 @@ const Login = () => {
     loginGoogle()
       .then((user) => {
         console.log(user);
-        history.replace('/');
+        history.replace("/");
       })
       .catch((err) => {
         console.log(err);
@@ -67,7 +65,7 @@ const Login = () => {
   const handleClickFacebook = () => {
     loginFacebook()
       .then((user) => {
-        history.replace('/');
+        history.replace("/");
       })
       .catch((err) => {
         console.log(err);
@@ -83,7 +81,7 @@ const Login = () => {
 
   const validateEmail = (email) => {
     const EMAIL_REGEX = new RegExp(
-      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
     );
     const emailIsValid = EMAIL_REGEX.test(email);
     if (emailIsValid) {
@@ -95,8 +93,8 @@ const Login = () => {
     } else {
       setErrorMessage({
         message:
-          'Su email es incorrecto. Por favor incluya una @ en el correo electrónico',
-        type: 'email',
+          "Su email es incorrecto. Por favor incluya una @ en el correo electrónico",
+        type: "email",
       });
       setFormValid({
         ...formValid,
@@ -108,9 +106,9 @@ const Login = () => {
   const handleBlur = (e) => {
     const { target } = e;
     const { value, name } = target;
-    console.log(value, 'yo');
+    console.log(value, "yo");
     if (value.length === 0) {
-      if (name === 'email') {
+      if (name === "email") {
         setFormValid({
           ...formValid,
           email: false,
@@ -126,24 +124,20 @@ const Login = () => {
         type: name,
       });
     }
-    if (name === 'password') {
+    if (name === "password") {
       setFormValid({
         ...formValid,
         password: true,
       });
       setErrorMessage({ ...initialErrorMessageState });
     }
-    if (name === 'email') {
+    if (name === "email") {
       validateEmail(value);
-      setErrorMessage({ ...initialErrorMessageState });
     }
   };
-  console.log(mensaje, 'mensaje :)');
-  console.log(errorMessage.message, 'mensaje yo :(');
+
   const Login = (e) => {
     e.preventDefault();
-    console.log(mensaje, 'mensaje');
-    console.log(errorMessage.message, 'mensaje yo');
     iniciarSesion({
       email,
       password,
@@ -151,82 +145,91 @@ const Login = () => {
   };
 
   return (
-    <div className='Login'>
-      <h1 className='Login__title'>
-        <span className='title_hola'> Hola, </span>
-        <span className='title__message'> Inicia Sesión! </span>
-      </h1>
-      <form className='Login__form' onSubmit={Login}>
-        {errorMessage.message ? <p>{errorMessage.message}</p> : ''}
-        <div className='form__group'>
-          <label htmlFor='email' className='form__label'>
-            Email :
-          </label>
-          <input
-            name='email'
-            type='email'
-            value={user.email}
-            className='form__input'
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            className={errorMessage.type === 'email' ? 'error' : ''}
-          />
-        </div>
-        <div className='form__group'>
-          <label htmlFor='password' className='form__label'>
-            Contraseña:
-          </label>
-          <input
-            name='password'
-            type='password'
-            value={user.password}
-            className='form__input'
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            className={errorMessage.type === 'password' ? 'error' : ''}
-          />
-          <div className='form__pass'>
-            <a href='/recovery-pass' className='form__link'>
-              Recuperar Contraseña
-            </a>
+    <div className="view-login">
+     <div className="box-login-img">
+     <div  className="box-login">
+        <div className="Login">
+          <h1 className="Login__title">
+            {/* <span className="title_hola"> Hola, </span> */}
+            <span className="title__message"> Iniciar Sesión </span>
+          </h1>
+          <form className="Login__form" onSubmit={Login}>
+            {errorMessage.message ? <div className="error-msj">{errorMessage.message}</div> : ""}
+            <div className="form__group">
+              <label htmlFor="email" className="form__label">
+                Email :
+              </label>
+              <input
+                name="email"
+                type="email"
+                value={user.email}
+                className="form__input"
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                className={errorMessage.type === "email" ? "error" : ""}
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="password" className="form__label">
+                Contraseña:
+              </label>
+              <input
+                name="password"
+                type="password"
+                value={user.password}
+                className="form__input"
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                className={errorMessage.type === "password" ? "error" : ""}
+              />
+              <div className="form__pass">
+                <a href="/recovery-pass" className="form__link">
+                  Recuperar Contraseña
+                </a>
+              </div>
+              <div className="form__check ">
+                <input type="checkbox" className="w-auto mr-2" />
+                <span className="form__check_s"> Mantenerme conectado </span>
+              </div>
+            </div>
+            <div className="form__group">
+              <button
+                type="submit"
+                className="form__button"
+                // disabled={!(formValid.email && formValid.password)}
+              >
+                Entrar
+              </button>
+              <p className="form__register">
+                No tiene cuenta registrate
+                <a href="/signup"> aquí. </a>
+              </p>
+            </div>
+          </form>
+          <div>
+            <div className="social__group">
+              <button
+                type="button"
+                className="form__button_other loginBtn--facebook"
+                onClick={handleClickFacebook}
+              >
+                Ingresar con Facebook
+              </button>
+              <button
+                type="button"
+                className="form__button_other loginBtn--google"
+                onClick={handleClickGoogle}
+              >
+                Ingresar con Google
+              </button>
+            </div>
           </div>
-          <div className='form__check '>
-            <input type='checkbox' />
-            <span className='form__check_s'> Mantenerme conectado </span>
-          </div>
-        </div>
-        <div className='form__group'>
-          <button
-            type='submit'
-            className='form__button'
-            // disabled={!(formValid.email && formValid.password)}
-          >
-            Entrar
-          </button>
-          <p className='form__register'>
-            No tiene cuenta registrate
-            <a href='/signup'> aquí. </a>
-          </p>
-        </div>
-      </form>
-      <div>
-        <div className='social__group'>
-          <button
-            type='button'
-            className='form__button_other'
-            onClick={handleClickFacebook}
-          >
-            Ingresar con Facebook
-          </button>
-          <button
-            type='button'
-            className='form__button_other'
-            onClick={handleClickGoogle}
-          >
-            > Ingresar con Google
-          </button>
         </div>
       </div>
+      <div className="box-img">
+        <img src={fondo} className=""/>
+      </div>
+     </div>
     </div>
   );
 };
