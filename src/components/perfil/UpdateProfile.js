9027daf4877c9f429f04bdde22ea115797a367a7
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-03-11 21:17:22 -0500

    maquetado de editar perfil

--- src/components/perfil/UpdateProfile.js
+++ src/components/perfil/UpdateProfile.js
@@ -17,7 +17,6 @@ const UpdateProfile = () => {
   const updatePassword = (e) => {
     e.preventDefault();
     if (newPassword === repeatNewPassword) { updatePasswordFirebase(newPassword); }
-    // alert(`Sen envio este password ${currentPassword}`);
     // validateCurrentPassword(usuario.email, currentPassword);
 
   };
@@ -34,30 +33,108 @@ const UpdateProfile = () => {
   // };
   return (
     <div className='container__profile'>
-      <h3>Editar perfil</h3>
       <div className='container__updateProfile'>
+        <h3>Editar perfil</h3>
         <form className='updateProfile__form' onSubmit={updateAllProfile}>
-          {usuario.photoUrl === '' ? (
-            <Avatar />
-          ) : (
-            <img src={usuario.photoUrl} className='user-img' />
-          )}
-          <input type='file' />
-          <input type='text' placeholder='Nick de usuario' />
-          <input type='email' placeholder='example@gmail.com' />
-          <input type='text' />
-          <input type='date' />
-          <input type='text' placeholder='pais' />
-          <input type='text' placeholder='área de conocimiento' />
-          <input type='text' placeholder='facebook' />
-          <input type='text' placeholder='github' />
-          <input type='text' placeholder='linkedin' />
-          <input type='text' placeholder='twitter' />
-          <textarea name='' id='' cols='30' rows='10' placeholder='bibliografia' />
-          <button type='submit'>Guardar Cambios</button>
+          <div className='container__updateProfileImg-flex'>
+            {usuario.photoUrl === '' ? (
+              <Avatar />
+            ) : (
+              <img src={usuario.photoUrl} className='user-img' />
+            )}
+            <input type='file' />
+          </div>
+          <div className='container__updateProfile-flex'>
+            <div>
+              <label htmlFor='name' className='form__label'>
+                Nick de Usuario :
+              </label>
+              <input type='text' id='name' name='name' placeholder='Nick de usuario' className='form__input' />
+            </div>
+            <div>
+              <label htmlFor='email' className='form__label'>
+                Correo Electrónico:
+              </label>
+              <input type='email' id='email' name='email' placeholder='example@gmail.com' className='form__input' />
+            </div>
+          </div>
+          <div className='container__updateProfile-flex'>
+            <div>
+              <label htmlFor='gender' className='form__label'>
+                Género
+              </label>
+              <select name='gender' id='gender' className='form__select'>
+                <option value=''>Hombre</option>
+                <option value=''>Mujer</option>
+                <option value=''>Otro</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor='birth' className='form__label'>
+                Fecha de Nacimiento
+              </label>
+              <input type='date' id='birth' name='birth' className='form__input' />
+            </div>
+          </div>
+          <div className='container__updateProfile-flex'>
+            <div>
+              <label htmlFor='country' className='form__label'>
+                País
+              </label>
+              <select name='country' id='country' className='form__select'>
+                <option value=''>Perú</option>
+                <option value=''>Colombia</option>
+                <option value=''>México</option>
+                <option value=''>Venezuela</option>
+                <option value=''>Argentina</option>
+                <option value=''>Bolivia</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor='area' className='form__label'>
+                Área de Conocimiento
+              </label>
+              <select name='area' id='area' className='form__select'>
+                <option value=''>Frontend</option>
+                <option value=''>Backend</option>
+                <option value=''>DevOps</option>
+                <option value=''>Video Game Developers</option>
+                <option value=''>UI/UX</option>
+                <option value=''>Database Developer</option>
+                <option value=''>Cloud Computing</option>
+              </select>
+            </div>
+          </div>
+          <div className='container__updateProfile-flex margin-top'>
+            <label htmlFor='facebook' className='form__label'>
+              Facebook
+            </label>
+            <input type='text' id='facebook' name='facebook' placeholder='facebook' className='form__input' />
+            <label htmlFor='github' className='form__label'>
+              Github
+            </label>
+            <input type='text' id='github' name='github' placeholder='github' className='form__input' />
+          </div>
+          <div className='container__updateProfile-flex margin-top'>
+            <label htmlFor='linkedin' className='form__label'>
+              Linkedin
+            </label>
+            <input type='text' id='linkedin' name='linkedin' placeholder='linkedin' className='form__input' />
+            <label htmlFor='twitter' className='form__label'>
+              Twitter
+            </label>
+            <input type='text' id='twitter' name='twitter' placeholder='twitter' className='form__input' />
+          </div>
+
+          <label htmlFor='bibliography' className='form__label'>
+            Bibliografía
+          </label>
+          <textarea name='bibliography' id='bibliography' cols='30' rows='10' maxLength='120' className='form__textarea' />
+          <button type='submit' className='form__button'>Editar Perfil</button>
         </form>
       </div>
       <div className='container__updatePassword'>
+        <h3>Cambiar Contraseña</h3>
         <form className='updatePassword__form' onSubmit={updatePassword}>
           <p>
             {errorMessage.message && errorMessage.type === 'password' ? <p>{errorMessage.message}</p> : ''}
@@ -73,9 +150,21 @@ const UpdateProfile = () => {
             onChange={(e) => setCurrentPassword(e.target.value)}
             value={currentPassword}
           /> */}
-          <input type='password' name='newPassword' placeholder='Nueva Contraseña' onChange={(e) => setNewPassword(e.target.value)} value={newPassword} />
-          <input type='password' name='repeatNewPassword' placeholder='Repetir Nueva Contraseña' onChange={(e) => setRepeatNewPassword(e.target.value)} value={repeatNewPassword} />
-          <input type='submit' value='Guardar' />
+          <div className='container__updateProfile-flex'>
+            <div>
+              <label htmlFor='newPassword' className='form__label'>
+                Nueva Contraseña
+              </label>
+              <input type='password' name='newPassword' id='newPassword' placeholder='Nueva Contraseña' onChange={(e) => setNewPassword(e.target.value)} value={newPassword} className='form__input' />
+            </div>
+            <div>
+              <label htmlFor='repeatNewPassword' className='form__label'>
+                Repetir Nueva Contraseña
+              </label>
+              <input type='password' name='repeatNewPassword' id='repeatNewPassword' placeholder='Repetir Nueva Contraseña' onChange={(e) => setRepeatNewPassword(e.target.value)} value={repeatNewPassword} className='form__input' />
+            </div>
+          </div>
+          <button type='submit' className='form__button'>Cambiar Contraseña</button>
         </form>
       </div>
     </div>
