12b2d8c7f6053e41fad7f7c6fa6a28a6ec589f3b
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-03-15 01:32:39 -0500

    estilos home

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './assets/styles/index.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('App'));
