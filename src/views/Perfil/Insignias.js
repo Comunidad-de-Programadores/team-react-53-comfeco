82b879c059b67f5052e2b70c65d2af50c44807d4
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-03-17 20:51:51 -0500

    llamando la colección de insignias en firebase

--- src/views/Perfil/Insignias.js
+++ src/views/Perfil/Insignias.js
@@ -1,95 +1,58 @@
-import React from 'react';
-import { Card, Button, CardDeck, Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import insignia_perfil from '../../assets/img/icon/insignia_perfil.svg';
 import insignia_social from '../../assets/img/icon/insignia_social.svg';
 import insignia_intelectual from '../../assets/img/icon/insignia_intelectual.svg';
 import insignia_solidario from '../../assets/img/icon/insignia_solidario.svg';
+import { getBadge } from '../../firebase/client';
 import '../../assets/styles/components/insignias.css';
 // import Card from 'react-bootstrap/Card';
 // import CardDeck from 'react-bootstrap/CardDeck';
 
 const Insignias = () => {
+
+  const [listBadge, setListBadge] = useState([]);
+
+  useEffect(() => {
+    getBadge()
+      .then((res) => {
+        setListBadge(res);
+      })
+      .catch((error) => {
+        console.log(error, 'error al mostrar insignias');
+      });
+
+  }, []);
+
   return (
     <div className='container__badge'>
       <h2 className='container__badge-title'>Insignias</h2>
       <Container>
         <Row>
-          <Col xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Card>
-              <Card.Img variant='top' src={insignia_perfil} />
-              <Card.Body>
-                <Card.Title>Sociable</Card.Title>
-                <Card.Text>
-                  <h5>Descripción</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-                <hr />
-                <Card.Text>
-                  <h5>¿Como ganarla?</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Card>
-              <Card.Img variant='top' src={insignia_social} />
-              <Card.Body>
-                <Card.Title>Publicista</Card.Title>
-                <Card.Text>
-                  <h5>Descripción</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-                <hr />
-                <Card.Text>
-                  <h5>¿Como ganarla?</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Card>
-              <Card.Img variant='top' src={insignia_intelectual} />
-              <Card.Body>
-                <Card.Title>Intelectual</Card.Title>
-                <Card.Text>
-                  <h5>Descripción</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-                <hr />
-                <Card.Text>
-                  <h5>¿Como ganarla?</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Card>
-              <Card.Img variant='top' src={insignia_solidario} />
-              <Card.Body>
-                <Card.Title>Solidario</Card.Title>
-                <Card.Text>
-                  <h5>Descripción</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-                <hr />
-                <Card.Text>
-                  <h5>¿Como ganarla?</h5>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+          {
+            listBadge.map((badge) => (
+              <Col xs={12} sm={6} md={4} lg={3} xl={3}>
+                <Card>
+                  <Card.Img variant='top' src={badge.image} />
+                  <Card.Body>
+                    <Card.Title>
+                      {badge.name}
+                    </Card.Title>
+                    <Card.Text>
+                      <h5>Descripción</h5>
+                      {badge.description}
+                    </Card.Text>
+                    <hr />
+                    <Card.Text>
+                      <h5>¿Como ganarla?</h5>
+                      {badge.instructions}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }
+
         </Row>
       </Container>
 
