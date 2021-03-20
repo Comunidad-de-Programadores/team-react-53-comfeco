210da812d6dd4a02ec323c89ae11f1c69ac6f417
NardaC <nardac6@gmail.com>
2021-03-20 11:41:06 -0500

    css

--- src/views/Perfil/Perfil.js
+++ src/views/Perfil/Perfil.js
@@ -1,90 +1,90 @@
-import React, { useContext, useState, useEffect } from 'react';
-import AuthContext from '../../auth/AuthContext';
-import Avatar from '../../components/Avatar';
-import UpdateProfile from '../../components/perfil/UpdateProfile';
-import EventsProfile from '../../components/perfil/EventsProfile';
-import { getBadgeSpecific } from '../../firebase/client';
-import Activity from '../../components/perfil/Activity';
+import React, { useContext, useState, useEffect } from "react";
+import AuthContext from "../../auth/AuthContext";
+import Avatar from "../../components/Avatar";
+import UpdateProfile from "../../components/perfil/UpdateProfile";
+import EventsProfile from "../../components/perfil/EventsProfile";
+import { getBadgeSpecific } from "../../firebase/client";
+import Activity from "../../components/perfil/Activity";
 
 const Perfil = () => {
   const { usuario, updateProfile, showUpdateProfile } = useContext(AuthContext);
   const [listBadge, setListBadge] = useState([]);
 
   useEffect(() => {
-    if (usuario.badge.indexOf('insignia_1') >= 0) {
-      getBadgeSpecific('insignia_1')
+    if (usuario.badge.indexOf("insignia_1") >= 0) {
+      getBadgeSpecific("insignia_1")
         .then((res) => {
           setListBadge(res);
         })
         .catch((error) => {
-          console.log(error, 'error al mostrar insignias');
+          console.log(error, "error al mostrar insignias");
         });
     }
   }, []);
 
   return (
-    <div className=' fade-in animated container-comfeco'>
-      <div className='box-perfil'>
+    <div className=" fade-in animated container-comfeco">
+      <div className="box-perfil">
         {updateProfile === false ? (
           <>
-            <div className='box-user bg-user'>
-              <div className='box-info-user'>
+            <div className="box-user bg-user">
+              <div className="box-info-user">
                 <a
-                  className='link-editar link-ancor'
+                  className="link-editar link-ancor"
                   onClick={showUpdateProfile}
                 >
                   Editar Perfil
                 </a>
-                <div className='circle-user'>
-                  {usuario.photoUrl === '' ? (
-                    <Avatar size='large' />
+                <div className="circle-user">
+                  {usuario.photoUrl === "" ? (
+                    <Avatar size="large" />
                   ) : (
-                    <img src={usuario.photoUrl} className='user-img' />
+                    <img src={usuario.photoUrl} className="user-img" />
                   )}
                 </div>
-                <h2 className='user-name'>{usuario.name}</h2>
-                <h4 className='user-area'>{usuario.area}</h4>
+                <h2 className="user-name">{usuario.name}</h2>
+                <h4 className="user-area">{usuario.area}</h4>
                 <span />
                 <p>{usuario.bibliography}</p>
-                {usuario.facebook !== '' ||
-                  usuario.github !== '' ||
-                  usuario.linkedin !== '' ||
-                  usuario.twitter !== '' ? (
-                  <div className='box-redes-user'>
-                    {usuario.facebook !== '' && (
+                {usuario.facebook !== "" ||
+                usuario.github !== "" ||
+                usuario.linkedin !== "" ||
+                usuario.twitter !== "" ? (
+                  <div className="box-redes-user">
+                    {usuario.facebook !== "" && (
                       <a
                         href={`https://www.facebook.com/${usuario.facebook}`}
-                        className='redes-user'
-                        target='_blank'
+                        className="redes-user"
+                        target="_blank"
                       >
-                        <i className='fab fa-facebook-f' />
+                        <i className="fab fa-facebook-f" />
                       </a>
                     )}
-                    {usuario.github !== '' && (
+                    {usuario.github !== "" && (
                       <a
                         href={`https://github.com/${usuario.github}`}
-                        className='redes-user'
-                        target='_blank'
+                        className="redes-user"
+                        target="_blank"
                       >
-                        <i className='fab fa-github' />
+                        <i className="fab fa-github" />
                       </a>
                     )}
-                    {usuario.linkedin !== '' && (
+                    {usuario.linkedin !== "" && (
                       <a
                         href={`https://www.linkedin.com/in/${usuario.linkedin}`}
-                        className='redes-user'
-                        target='_blank'
+                        className="redes-user"
+                        target="_blank"
                       >
-                        <i className='fab fa-linkedin-in' />
+                        <i className="fab fa-linkedin-in" />
                       </a>
                     )}
-                    {usuario.twitter !== '' && (
+                    {usuario.twitter !== "" && (
                       <a
                         href={`https://twitter.com/${usuario.twitter}`}
-                        className='redes-user'
-                        target='_blank'
+                        className="redes-user"
+                        target="_blank"
                       >
-                        <i className='fab fa-twitter' />
+                        <i className="fab fa-twitter" />
                       </a>
                     )}
                   </div>
@@ -93,24 +93,24 @@ const Perfil = () => {
                 )}
               </div>
             </div>
-            <div className='box-centro'>
-              <div className='box-centro-insignia'>
-                <h4 className='user-name'> Mis insignias</h4>
+            <div className="box-centro">
+              <div className="box-centro-insignia">
+                <h4 className="user-name"> Mis insignias</h4>
                 {usuario.badge.length === 0 ? (
                   <p>Actualmente no cuentas con ninguna insignia</p>
                 ) : (
-                  <div className='box-all-insignias'>
+                  <div className="box-all-insignias">
                     {listBadge.map((badge) => (
-                      <div className='box-insignia'>
-                        <img src={badge.image} className='insignia-img' />
+                      <div className="box-insignia">
+                        <img src={badge.image} className="insignia-img" />
                       </div>
                     ))}
                   </div>
                 )}
               </div>
-              <div className='box-centro-actividad'>
-                <h4 className='user-name'> Actividad reciente</h4>
-                <div className='box-all-activity'>
+              <div className="box-centro-actividad">
+                <h4 className="user-name"> Actividad reciente</h4>
+                <div className="box-all-activity">
                   {usuario.activity.length > 0 ? (
                     usuario.activity.map((item) => {
                       return (
@@ -124,12 +124,12 @@ const Perfil = () => {
                       );
                     })
                   ) : (
-                    <div className='no-activites'>
-                      <div className='box-activities'>
-                        {' '}
-                        <span className='emoticon'>
-                          {' '}
-                          <i className='far fa-frown-open fa-5x' />
+                    <div className="no-activites">
+                      <div className="box-activities">
+                        {" "}
+                        <span className="emoticon">
+                          {" "}
+                          <i className="far fa-frown-open fa-5x" />
                         </span>
                         <p>
                           Lastimosamente aún no tienes actividad, ¿qué tal si
@@ -141,7 +141,7 @@ const Perfil = () => {
                 </div>
               </div>
             </div>
-            <div className='box-user bg-user'>
+            <div className="box-user bg-user">
               <EventsProfile />
             </div>
           </>
